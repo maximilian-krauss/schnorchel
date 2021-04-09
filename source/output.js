a336f92a65530b8752d8ec4366051aa0042fd3c5
Maximilian Krauss <maximilian.krauss@share-now.com>
2021-04-09 08:06:54 +0200

    feat: Refactoring

--- source/output.js
+++ source/output.js
@@ -1,30 +1,28 @@
 const path = require('path')
 const fs = require('fs')
 
-async function writeToOutputDirectory (schemaName, schema, config) {
-  const {
-    logger,
-    output: {
-      directory,
-      schemaSerializer,
-      schemaFileNameFormatter
-    }
-  } = config
-  const destinationDirectory = path.resolve(directory)
-  await fs.promises.mkdir(destinationDirectory, { recursive: true })
-
-  const serializedSchema = await schemaSerializer(schema)
-  const filename = schemaFileNameFormatter(schemaName, config)
-
-  await fs.promises.writeFile(
-    path.join(destinationDirectory, filename),
-    serializedSchema,
-    { encoding: 'utf8' }
-  )
-
-  logger.info('Schema saved to disk', { filename, destinationDirectory })
-}
-
 module.exports = {
-  writeToOutputDirectory
+  async writeToOutputDirectory (schemaName, schema, config) {
+    const {
+      logger,
+      output: {
+        directory,
+        schemaSerializer,
+        schemaFileNameFormatter
+      }
+    } = config
+    const destinationDirectory = path.resolve(directory)
+    await fs.promises.mkdir(destinationDirectory, { recursive: true })
+
+    const serializedSchema = await schemaSerializer(schema)
+    const filename = schemaFileNameFormatter(schemaName, config)
+
+    await fs.promises.writeFile(
+      path.join(destinationDirectory, filename),
+      serializedSchema,
+      { encoding: 'utf8' }
+    )
+
+    logger.info('Schema saved to disk', { filename, destinationDirectory })
+  }
 }
